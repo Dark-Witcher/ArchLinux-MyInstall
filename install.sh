70e9828ca2023e68da457d2ddd84daf1eb2fca5d
Dark Witcher <Dark-Witcher@users.noreply.github.com>
2024-11-17 14:31:42 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install Hyprland and all required software
+
+yay -Sy oh-my-posh
+
+cp .bashrc ~/
+
+cp .aliases ~/
+
+cp -r alacritty ~/.config/
+
+cp -r fastfetch ~/.config/
+
+cp -r hypr ~/.config/
+
+cp -r oh-my-posh ~/.config/
+
+cp -r swaylock ~/.config/
+
+cp -r waybar ~/.config/
+
+cp -r wlogout ~/.config/
+
+cp -r scripts ~/.config/
